914fb67027349d9a4a821d0d6bbcd4b6a0120a72
fstufop <fstufop@gmail.com>
2021-09-25 10:18:23 -0300

    feat: add 'tudum'

--- NerdFlix/NerdFlix/Sources/AppDelegate.swift
+++ NerdFlix/NerdFlix/Sources/AppDelegate.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    var window: UIWindow?
+   
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+        
+        let viewController = HomeViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        window = UIWindow.init(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
 }
